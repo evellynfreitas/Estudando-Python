1dfd4fb14743e5f5c4eaba12e2c4052c34db55a8
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-06-07 15:07:24 -0300

    Comando type

--- Cursos/Python Básico/modulo3.py
+++ Cursos/Python Básico/modulo3.py
@@ -2,9 +2,16 @@ print("Tipos de dados")
 
 # int
 codigo = 10
+print(codigo, "é do tipo:", type(codigo))
+
 # float
 salario = 1500.00
+print(salario, "é do tipo:",type(salario))
+
 # string
 nome = "Jose"
+print(nome, "é do tipo:",type(nome))
+
 # boolean
 contratado = True
+print(contratado, "é do tipo:",type(contratado))
