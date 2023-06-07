a8658ecd3cf148d5b37b1edb912f4aeb20707f63
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-06-07 15:30:15 -0300

    Operadores

---
+++ Cursos/Python Básico/modulo4.py
@@ -0,0 +1,12 @@
+A = 3
+B = 5
+C = 10
+D = 15
+E = 3
+
+print("A é igual a B? ", A == B)
+print("Negação de A é igual a B? ", not A == B)
+print("A é menor que B e C? ", A < B and A < C)
+print("C é divisível por B? ", C % B == 0)
+print("D é divisível por A e B? ", D % B == 0 and D % A == 0)
+print("E é igual a A ou a B?", E == A or E == B)
