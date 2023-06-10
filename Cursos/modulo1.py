460c053c9146e06940ec8cd858423ab09c58ef4f
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-06-09 22:29:03 -0300

    Add files via upload

---
+++ Cursos/modulo1.py
@@ -0,0 +1,25 @@
+#exercicio 2
+
+n = 7
+print("Eu pulei as",n,"ondas.")
+print("Foi durante o reveillon.")
+
+#exercicio 3
+
+pi = 3.141559265359
+r = 6378.1
+p = 2 * pi * r
+
+print("\nO perímetro da Terra no equador é",p)
+
+#exercicio 4
+
+milhaTerrestre = 1.60934
+milhaKm = 13 * milhaTerrestre
+
+print("\nMilhas Terrestres:",milhaKm)
+
+milhaNautica = 1.852
+milhaKm = 13 * milhaNautica
+
+print("\nMilhas Náuticas:",milhaKm)
